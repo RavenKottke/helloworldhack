d2ef367a60072de49cfd5b315648a146bb1c3bcd
RavenKottke <42457901+RavenKottke@users.noreply.github.com>
2018-08-23 10:11:57 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Raven Kottke & Zavaire Ingram
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Raven Kottke- Computer Science\n");
+  printf("Zavaire Ingram- Computer Science\n");
+  
+  return 0;
+}
